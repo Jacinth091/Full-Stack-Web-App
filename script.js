288417cdfff89310fb39c19aeab8c4dd96b6b8ad
Jacinth <jacinthcedricbarral@gmail.com>
2026-02-14 08:48:57 +0800

    feat[Jacinth]: Added a DOM manipulation for the request modal, enabling it to add item row and remove item row

--- script.js
+++ script.js
@@ -25,3 +25,38 @@ function handleRouting() {
 }
 
 function setAuthState(isAuth, user) {}
+
+// const inputButton = document.getElementById("modalButton");
+// const modalDialog = document.getElementById("requestModal");
+
+// modalDialog.addEventListener("shown.bs.modal", () => {
+//   console.log("Pressed");
+//   inputButton.focus();
+// });\
+
+function addItem() {
+  const container = document.getElementById("item-container");
+  const newItemRow = `
+  <div class="d-flex align-items-center gap-1 w-100 item-row"> 
+    <div class="flex-grow-1">
+      <input type="text" class="form-control outline-none" placeholder="Item">
+    </div>
+    <div class="w-25">
+      <input type="number" class="form-control" placeholder="Qty">
+    </div>
+    <div class=""> <button type="button" class="btn btn-danger remove-btn">&times;</button>
+    </div>
+  </div>`;
+  container.insertAdjacentHTML("beforeend", newItemRow);
+}
+
+document
+  .getElementById("item-container")
+  .addEventListener("click", function (e) {
+    console.log("hello");
+    const button = e.target.classList.contains("remove-btn");
+    if (button) {
+      const row = e.target.closest(".item-row");
+      row.remove();
+    }
+  });
